07dbcea16e7128da92c5a26d539678a027cf5143
marzio civiero <mciviero30@gmail.com>
2025-12-13 04:07:07 +0000

    File changes

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -902,11 +902,12 @@ export default function Chat() {
                     >
                       <Send className="w-5 h-5" />
                     </Button>
-                  </div>
-                </form>
-            </div>
-          </div>
-        </div>
+                    </div>
+                    </form>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
 
         {/* New Direct Message Dialog */}
         <Dialog open={showNewDM} onOpenChange={setShowNewDM}>
