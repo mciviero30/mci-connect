bf877b96f557882c31c66bac358000fa5c8db835
marzio civiero <mciviero30@gmail.com>
2025-12-17 16:52:56 +0000

    File changes

--- src/pages/Nomina.jsx
+++ src/pages/Nomina.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
-import { Banknote, Download, Search, Edit, User } from "lucide-react";
+import { Banknote, Download, Search, Edit, User, FileText } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
