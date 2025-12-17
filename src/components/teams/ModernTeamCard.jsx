4380e2c697cfeadb028f77ba7b03ffc1014bb749
marzio civiero <mciviero30@gmail.com>
2025-12-17 04:52:06 +0000

    File changes

--- src/components/teams/ModernTeamCard.jsx
+++ src/components/teams/ModernTeamCard.jsx
@@ -91,18 +91,8 @@ export default function ModernTeamCard({ team, stats, onViewDetails }) {
         </div>
       </div>
 
-      {/* Add New Button - Full Width at Bottom */}
-      <div 
-        className="bg-gradient-to-r from-[#1E6FE8] to-[#0052CC] h-[40px] cursor-pointer hover:from-[#1557C0] hover:to-[#003d99] transition-all flex items-center justify-center" 
-        onClick={() => onViewDetails(team)}
-      >
-        <div className="flex items-center justify-center gap-1.5">
-          <span className="text-white font-bold text-[12px]">Add New</span>
-          <div className="w-5 h-5 bg-white rounded-full flex items-center justify-center flex-shrink-0">
-            <Plus className="w-3 h-3 text-[#1E6FE8]" />
-          </div>
-        </div>
-      </div>
+      {/* Gradient Line at Bottom */}
+      <div className="h-[3px] w-full bg-gradient-to-r from-[#1E6FE8] to-[#0052CC]" />
     </Card>
   );
 }
