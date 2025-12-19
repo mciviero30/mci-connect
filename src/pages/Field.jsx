3b6e2eeb2b1f1a01bcb046108337054d893bcf01
marzio civiero <mciviero30@gmail.com>
2025-12-19 07:50:28 +0000

    Logo Quotes Invoices

--- src/pages/Field.jsx
+++ src/pages/Field.jsx
@@ -141,11 +141,11 @@ export default function Field() {
     <div className="min-h-screen bg-[#FAFAFA] dark:bg-[#181818]">
       {/* Header */}
       <div className="px-0 py-0">
-        <div className="w-full mb-3 flex justify-center">
+        <div className="w-full mb-3">
           <img 
-            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68ee5191fb756d843d0561d3/36077bd2e_Screenshot2025-12-19at24801AM.png"
+            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68ee5191fb756d843d0561d3/5717f3e58_Screenshot2025-12-19at24110AM.png"
             alt="MCI Field"
-            className="w-[90%] h-auto object-contain"
+            className="w-full h-auto object-cover"
             style={{ imageRendering: 'crisp-edges', imageRendering: '-webkit-optimize-contrast' }}
           />
         </div>
