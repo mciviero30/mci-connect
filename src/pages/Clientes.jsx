aaa9a4f705ffbe2a13480e8b1b027a0263c0d4d3
marzio civiero <mciviero30@gmail.com>
2025-12-28 07:36:57 +0000

    File changes

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -180,8 +180,8 @@ export default function Clientes() {
   };
 
   return (
-    <div className="p-4 md:p-8 min-h-screen bg-[#F1F5F9] dark:bg-[#181818]">
-      <div className="max-w-7xl mx-auto">
+    <div className="min-h-screen bg-[#F1F5F9] dark:bg-[#181818] pb-20 md:pb-0">
+      <div className="max-w-7xl mx-auto p-4 md:p-8">
         <PageHeader
           title={t('customers')}
           description={`${sortedCustomers.length} ${t('customers').toLowerCase()}`}
