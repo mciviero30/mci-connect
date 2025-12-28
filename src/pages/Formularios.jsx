84c19d465c2bb13c41269bf2a6919f69f1385f6c
marzio civiero <mciviero30@gmail.com>
2025-12-28 07:55:15 +0000

    File changes

--- src/pages/Formularios.jsx
+++ src/pages/Formularios.jsx
@@ -76,8 +76,8 @@ export default function Formularios() {
   );
 
   return (
-    <div className="min-h-screen p-4 md:p-8 bg-[#FAFAFA] dark:bg-[#181818]">
-      <div className="max-w-7xl mx-auto">
+    <div className="min-h-screen bg-[#FAFAFA] dark:bg-[#181818] pb-20 md:pb-0">
+      <div className="max-w-7xl mx-auto p-4 md:p-8">
         <PageHeader
           title={t('forms')}
           description={`${availableTemplates.length} ${t('forms').toLowerCase()}`}
