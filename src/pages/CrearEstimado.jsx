537c3fa9d61114c39cd678260c01fd2a0db8f855
marzio civiero <mciviero30@gmail.com>
2025-12-29 21:11:50 +0000

    File changes

--- src/pages/CrearEstimado.jsx
+++ src/pages/CrearEstimado.jsx
@@ -693,9 +693,9 @@ Use realistic driving estimates. Round distance to 1 decimal, time to nearest 0.
               <div className="hidden md:grid md:grid-cols-12 gap-2 px-4 py-2 bg-slate-100 rounded-t-lg border border-slate-200 text-xs font-semibold text-slate-600">
                 <div className="col-span-3">Item</div>
                 <div className="col-span-3">{t('description')}</div>
-                <div className="col-span-1 text-center">{t('quantity')}</div>
+                <div className="col-span-2 text-center">{language === 'es' ? 'Techs/Qty • Días/Hrs' : 'Techs/Qty • Days/Hrs'}</div>
                 <div className="col-span-1 text-center">Unit</div>
-                <div className="col-span-2 text-center">{t('unitPrice')}</div>
+                <div className="col-span-1 text-center">{t('unitPrice')}</div>
                 <div className="col-span-1 text-right">{t('total')}</div>
                 <div className="col-span-1"></div>
               </div>
