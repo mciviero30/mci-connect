9221084e21a190a6a9267f50642f942846f11416
marzio civiero <mciviero30@gmail.com>
2026-01-13 18:28:55 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -369,7 +369,6 @@ const LayoutContent = ({ children, currentPageName, user, isLoading, error }) =>
       icon: Users,
       items: [
         { title: 'Employees', url: createPageUrl("Empleados"), icon: Users },
-        { title: 'Employee Import', url: createPageUrl("EmployeeImport"), icon: Users },
         { title: 'Teams', url: createPageUrl("Teams"), icon: MapPin },
         { title: 'Performance', url: createPageUrl("PerformanceManagement"), icon: Award },
         { title: 'Skill Matrix', url: createPageUrl("SkillMatrix"), icon: Award },
