493e932d2d8d1f15aa8c37491a45ddcc9af5c74f
marzio civiero <mciviero30@gmail.com>
2026-02-02 20:11:34 +0000

    File changes

--- src/components/field/FieldPlansView.jsx
+++ src/components/field/FieldPlansView.jsx
@@ -18,6 +18,7 @@ import BatchPlanUploadDialog from './BatchPlanUploadDialog.jsx';
 import { Badge } from '@/components/ui/badge';
 import PlanGridSection from './PlanGridSection.jsx';
 import PlanSectionAccordion from './PlanSectionAccordion.jsx';
+import PendingPlansAlert from './PendingPlansAlert.jsx';
 
 const MAX_FILE_SIZE_MB = 100;
 const WARNING_FILE_SIZE_MB = 50;
