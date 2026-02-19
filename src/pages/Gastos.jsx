da216c43e0fe872151512fb746dbd86bb2508751
marzio civiero <mciviero30@gmail.com>
2026-02-19 03:05:18 +0000

    File changes

--- src/pages/Gastos.jsx
+++ src/pages/Gastos.jsx
@@ -210,7 +210,7 @@ export default function Gastos() {
 
         {/* Expense Form Dialog */}
         <Dialog open={showExpenseForm} onOpenChange={setShowExpenseForm}>
-          <DialogContent className="max-w-3xl max-h-[90vh] overflow-y-auto bg-white dark:bg-[#282828] border-slate-200 dark:border-slate-700">
+          <DialogContent className="max-w-3xl max-h-[90vh] overflow-y-auto bg-white dark:bg-slate-800 border-slate-200 dark:border-slate-700">
             <DialogHeader>
               <DialogTitle className="text-slate-900 dark:text-white">
                 {t('newExpense')} - Company Expense
