aedb03aaebbc549edd8a5c2062f8bde005b59c57
marzio civiero <mciviero30@gmail.com>
2026-03-07 22:25:25 +0000

    File changes

--- functions/autoCalculateInvoiceBalance.ts
+++ functions/autoCalculateInvoiceBalance.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * W1 FIX: Auto-calculate Invoice balance field
