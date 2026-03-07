1f99ea0f4c9c2095e9ca1c240d1bf49ad54e09a7
marzio civiero <mciviero30@gmail.com>
2026-03-07 21:53:15 +0000

    File changes

--- functions/generatePayrollBatch.ts
+++ functions/generatePayrollBatch.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 function getISOWeekKey(date) {
   const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
