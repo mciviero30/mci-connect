710d74d73e38fb487ba0dd838e4372ba13b701e6
marzio civiero <mciviero30@gmail.com>
2026-03-21 14:06:52 +0000

    File changes

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -5,7 +5,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-[9px] font-medium leading-none text-slate-900 dark:text-slate-100 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-[8px] font-medium leading-none text-slate-900 dark:text-slate-100 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
 const Label = React.forwardRef(({ className, ...props }, ref) => (
