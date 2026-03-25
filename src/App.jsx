923643f2de6dddcbb73d7f779fd7d7bc237752f9
marzio civiero <mciviero30@gmail.com>
2026-03-25 20:04:35 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import TimeTrackingTestControl from '@/pages/TimeTrackingTestControl';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
-const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
+const MainPage = mainPageKey ? Pages[mainPageKey] : </>; 
 
 
 
