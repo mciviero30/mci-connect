689baab3eae519e0f8663d20b8af557d9fbc28fa
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:30:57 -0400

    chore: bump @base44/sdk to 0.8.20 in resetEmployeeToPending

--- base44/functions/resetEmployeeToPending/entry.ts
+++ base44/functions/resetEmployeeToPending/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * Reset an employee back to pending status with correct email
