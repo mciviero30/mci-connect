eb9954fde454901b07a872285bec569917b3806a
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:30:22 -0400

    chore: bump @base44/sdk to 0.8.20 in migratePendingToUser

--- base44/functions/migratePendingToUser/entry.ts
+++ base44/functions/migratePendingToUser/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * ONE-TIME MIGRATION: Merge PendingEmployee data into existing User records
