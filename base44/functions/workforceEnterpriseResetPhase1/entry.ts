35dfa9794dec15daa7bcb667913649d7c44797da
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:31:51 -0400

    chore: bump @base44/sdk to 0.8.20 in workforceEnterpriseResetPhase1

--- base44/functions/workforceEnterpriseResetPhase1/entry.ts
+++ base44/functions/workforceEnterpriseResetPhase1/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * PHASE 1: Delete ALL workforce data except admin/CEO user (mciviero30@gmail.com)
