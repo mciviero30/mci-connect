16edf83e14ec5602d59390a7a8ddbbab5082ad41
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:30:36 -0400

    chore: bump @base44/sdk to 0.8.20 in previewPayrollImport

--- base44/functions/previewPayrollImport/entry.ts
+++ base44/functions/previewPayrollImport/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 // Lightweight UUID v4 generator using Web Crypto (built-in to Deno)
 function uuidv4() {
