6e1440687aa0a8362a32642824c5cf130cd8441b
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:30:53 -0400

    chore: bump @base44/sdk to 0.8.20 in repairOrphanedJobsManual

--- base44/functions/repairOrphanedJobsManual/entry.ts
+++ base44/functions/repairOrphanedJobsManual/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * PHASE 2B: Manual orphan repair (safe, limited scope)
