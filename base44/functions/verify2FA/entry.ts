29ce5261fd47a1128484c5e8eed7ae3c523f3031
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:31:47 -0400

    chore: bump @base44/sdk to 0.8.20 in verify2FA

--- base44/functions/verify2FA/entry.ts
+++ base44/functions/verify2FA/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 import * as OTPAuth from 'npm:otpauth@9.3.2';
 
 Deno.serve(async (req) => {
