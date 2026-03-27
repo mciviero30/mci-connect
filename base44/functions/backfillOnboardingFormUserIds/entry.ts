2dbbe3d0b2859c43f434c622927c7605e48ea3a8
mciviero30 <mciviero30@gmail.com>
2026-03-27 17:28:39 -0400

    chore: bump @base44/sdk to 0.8.20 in backfillOnboardingFormUserIds

--- base44/functions/backfillOnboardingFormUserIds/entry.ts
+++ base44/functions/backfillOnboardingFormUserIds/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 /**
  * ONBOARDINGFORM MIGRATION: Backfill user_id from employee_email
